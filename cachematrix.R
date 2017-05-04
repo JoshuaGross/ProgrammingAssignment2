3c4815c405ef37237ed7c86dcf6b3283fd009985
Joshua Gross <joshua.gross@gmail.com>
2017-05-03 20:58:14 -0700

    implement matrix caching

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  getMatrix     <- function ()  x 
+  inverse       <- function ()  cacheSolve(x)
+  getInverse    <- function ()  cachedInverse
+  setInverse    <- function (i) cachedInverse <<- i
+  
+  list(getMatrix = getMatrix, inverse = inverse, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (is.null(x$getInverse())) {
+    x$setInverse(solve(x$getMatrix(), ...))
+  }
+  
+  x$getInverse()
 }
+
+# test:
+# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# h8 <- hilbert(8)
+# cm <- makeCacheMatrix(h8)
